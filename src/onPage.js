ab77b1312bd5e3644e4e1a52853efaa111279767
tada-tsu <tada.purple.haze@gmail.com>
2019-01-25 11:18:29 +0900

    Update onPage script

--- src/onPage.js
+++ src/onPage.js
@@ -15,11 +15,13 @@ class YoutubeLiveComment {
     /**
      *
      * @typedef {Object} Comments
+     * @property {boolean} paid Paid comment (Super Chat)
      * @property {string} id Comment ID
      * @property {string} image Comment author image
      * @property {string} timestamp Comment timestamp
      * @property {string} author_name Comment author name
      * @property {string} message Comment body html tag
+     * @property {string} price Paid price with currency (Super Chat)
      * 
     */
     
@@ -32,17 +34,30 @@ class YoutubeLiveComment {
      */
     commentUpdate(returnJson = false) {
         this._newComments = []
-        const newComments = document.querySelector("#live-chat-iframe").contentDocument.querySelectorAll("yt-live-chat-text-message-renderer:not([data-catched])")
+        
+        const commentSelectors = {
+            normal: "yt-live-chat-text-message-renderer",
+            paid: "yt-live-chat-paid-message-renderer",
+        }
+        
+        const frame = document.querySelector("#live-chat-iframe") || document.querySelector("#chatframe")
+        const frameDocument = frame.contentDocument
+        
+        const newComments = frameDocument.querySelectorAll(`${commentSelectors.normal}:not([data-catched]), ${commentSelectors.paid}:not([data-catched])`)
 
         for (let i = 0; i < newComments.length; i++) {
             const comment = newComments[i];
 
+            const isPaid = comment.tagName == commentSelectors.paid.toUpperCase()
+            
             this._newComments.push({
+                paid: isPaid,
                 id: comment.id,
                 image: comment.querySelector("#author-photo img").src,
                 timestamp: comment.querySelector("#timestamp").textContent,
                 author_name: comment.querySelector("#author-name").textContent,
                 message: comment.querySelector("#message").innerHTML,
+                price: isPaid ? comment.querySelector("#purchase-amount").textContent : null,
             })
 
             comment.dataset.catched = true
@@ -92,3 +107,6 @@ class YoutubeLiveComment {
         }
     }
 }
+
+
+export default YoutubeLiveComment
