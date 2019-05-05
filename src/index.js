1a726d3a2c1721864af3146038c723cb3c051420
tada-tsu <tada.purple.haze@gmail.com>
2019-05-06 00:42:34 +0900

    remove pupetter

--- src/index.js
+++ src/index.js
@@ -1,108 +1,108 @@
-import puppeteer from "puppeteer"
-import YoutubeLiveCommentOnPage from "./onPage.js"
+// import puppeteer from "puppeteer"
+// import YoutubeLiveCommentOnPage from "./onPage.js"
 
 
-class YoutubeLiveComment {
-    /**
-     *Creates an instance of YoutubeLiveComment.
-     * @param {array} [cookie=[]]
-     * @memberof YoutubeLiveComment
-     */
-    constructor(cookie = []) {
-        this.dashBoardURL = "https://www.youtube.com/live_dashboard"
+// class YoutubeLiveComment {
+//     /**
+//      *Creates an instance of YoutubeLiveComment.
+//      * @param {array} [cookie=[]]
+//      * @memberof YoutubeLiveComment
+//      */
+//     constructor(cookie = []) {
+//         this.dashBoardURL = "https://www.youtube.com/live_dashboard"
 
-        if (cookie.length > 0) {
-            this.cookie = cookie
-        }
+//         if (cookie.length > 0) {
+//             this.cookie = cookie
+//         }
 
-        this.comments = []
-        this.newComments = []
+//         this.comments = []
+//         this.newComments = []
 
-    }
+//     }
 
-    /**
-     *
-     *
-     * @returns {Promise} 
-     * @memberof YoutubeLiveComment
-     */
-    initDashBoard() {
-        return new Promise(async resolve => {
-            this.mainBrowser = await puppeteer.launch()
+//     /**
+//      *
+//      *
+//      * @returns {Promise} 
+//      * @memberof YoutubeLiveComment
+//      */
+//     initDashBoard() {
+//         return new Promise(async resolve => {
+//             this.mainBrowser = await puppeteer.launch()
 
 
-            this.mainPage = (await this.mainBrowser.pages())[0]
-            if (typeof this.cookie != "undefined") {
-                await this.mainPage.setCookie(...this.cookie)
-            }
-            this.mainPage.once("load", async () => {
-                if (this.isLoginPage(this.mainPage.url())) {
-                    this.cookie = await this.login()
-                }
-                if (typeof this.cookie != "undefined") {
-                    await this.mainPage.setCookie(...this.cookie)
-                    await this.mainPage.goto(this.dashBoardURL)
-                }
+//             this.mainPage = (await this.mainBrowser.pages())[0]
+//             if (typeof this.cookie != "undefined") {
+//                 await this.mainPage.setCookie(...this.cookie)
+//             }
+//             this.mainPage.once("load", async () => {
+//                 if (this.isLoginPage(this.mainPage.url())) {
+//                     this.cookie = await this.login()
+//                 }
+//                 if (typeof this.cookie != "undefined") {
+//                     await this.mainPage.setCookie(...this.cookie)
+//                     await this.mainPage.goto(this.dashBoardURL)
+//                 }
 
-                await this.mainPage.evaluate((YoutubeLiveCommentOnPageString) => {
-                    return eval(`
-                        ${YoutubeLiveCommentOnPageString}
-                        window.YoutubeLiveComment = new YoutubeLiveComment() 
-                        `)
-                }, YoutubeLiveCommentOnPage.toString())
+//                 await this.mainPage.evaluate((YoutubeLiveCommentOnPageString) => {
+//                     return eval(`
+//                         ${YoutubeLiveCommentOnPageString}
+//                         window.YoutubeLiveComment = new YoutubeLiveComment() 
+//                         `)
+//                 }, YoutubeLiveCommentOnPage.toString())
 
-                resolve()
-            })
-            await this.mainPage.goto(this.dashBoardURL)
-        })
-    }
+//                 resolve()
+//             })
+//             await this.mainPage.goto(this.dashBoardURL)
+//         })
+//     }
 
-    async goDashBoard() {
-        return await this.mainPage.goto(this.dashBoardURL)
-    }
+//     async goDashBoard() {
+//         return await this.mainPage.goto(this.dashBoardURL)
+//     }
 
-    isLoginPage(url) {
-        return /^https:\/\/accounts\.google\.com\/ServiceLogin/.test(url) || /^https:\/\/accounts\.google\.com\/signin\//.test(url)
-    }
+//     isLoginPage(url) {
+//         return /^https:\/\/accounts\.google\.com\/ServiceLogin/.test(url) || /^https:\/\/accounts\.google\.com\/signin\//.test(url)
+//     }
 
-    login() {
-        return new Promise(async (resolve) => {
-            this.noHeadlessBrowser = await puppeteer.launch({
-                headless: false,
-            })
+//     login() {
+//         return new Promise(async (resolve) => {
+//             this.noHeadlessBrowser = await puppeteer.launch({
+//                 headless: false,
+//             })
 
-            const loggedInCheckHandler = () => {
-                if (this.loginPage.url() == this.dashBoardURL) {
-                    this.loginPage.cookies().then(async res => {
-                        await this.noHeadlessBrowser.close()
+//             const loggedInCheckHandler = () => {
+//                 if (this.loginPage.url() == this.dashBoardURL) {
+//                     this.loginPage.cookies().then(async res => {
+//                         await this.noHeadlessBrowser.close()
 
-                        resolve(res)
-                    })
-                } else {
-                    this.loginPage.once("load", loggedInCheckHandler)
-                }
-            }
+//                         resolve(res)
+//                     })
+//                 } else {
+//                     this.loginPage.once("load", loggedInCheckHandler)
+//                 }
+//             }
 
-            this.loginPage = (await this.noHeadlessBrowser.pages())[0]
-            this.loginPage.once("load", loggedInCheckHandler)
-            this.loginPage.goto(this.dashBoardURL)
-        })
-    }
+//             this.loginPage = (await this.noHeadlessBrowser.pages())[0]
+//             this.loginPage.once("load", loggedInCheckHandler)
+//             this.loginPage.goto(this.dashBoardURL)
+//         })
+//     }
 
-    async updateComment() {
-        this.newComments = await this.mainPage.evaluate(() => {
-            return YoutubeLiveComment.commentUpdate()
-        })
+//     async updateComment() {
+//         this.newComments = await this.mainPage.evaluate(() => {
+//             return YoutubeLiveComment.commentUpdate()
+//         })
 
-        this.comments = await this.mainPage.evaluate(() => {
-            return YoutubeLiveComment.getComments()
-        })
+//         this.comments = await this.mainPage.evaluate(() => {
+//             return YoutubeLiveComment.getComments()
+//         })
 
-        return this.comments
-    }
-}
+//         return this.comments
+//     }
+// }
 
-export default YoutubeLiveComment
-export {
-    YoutubeLiveCommentOnPage,
-} 
+// export default YoutubeLiveComment
+// export {
+//     YoutubeLiveCommentOnPage,
+// } 
